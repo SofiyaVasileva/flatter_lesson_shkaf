a103645f6539a7319280f1ffdcd80d713baa337d
SofiyaVasileva <91559612+SofiyaVasileva@users.noreply.github.com>
2024-02-28 13:47:58 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shkaf',
+      title: 'test',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(170, 161, 187, 1)),
         useMaterial3: true,
       ),
       home: HomePage(),
@@ -24,77 +24,167 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: const Color.fromARGB(255, 84, 85, 84), 
+      title: const Text('Column', style:
+       TextStyle(color: Color.fromARGB(198, 255, 255, 255)
+       )
+      ,) 
+      ),
       body: Container(
         color: Colors.grey,
-        child: Center(
-          child: Container(
-            color: Colors.white,
-            margin: const EdgeInsets.only(left: 30, right: 30),
-            child: Row(
-              mainAxisSize: MainAxisSize.min, // Добавлено свойство mainAxisSize
-              textDirection: TextDirection.ltr,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              verticalDirection: VerticalDirection.down,
-              children: [
-                Column(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        color: Colors.redAccent,
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Expanded(flex: 3,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 243, 196, 125),
+                      child: const Align(
+                         child: Text(
+                          "1", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
+                      ),
+                    ),               
+                    ),
+                  Expanded(flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 152, 229, 121),
+                      child: const Align(
+                        child: Text(
+                          "2", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.brown,
+                    
+                  ),
+                  Expanded(flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 110, 194, 233),
+                      child: const Align(
+                        child: Text(
+                          "3", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.brown,
+                    
+                  ),
+                  Expanded(flex: 3,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 144, 169, 84),
+                      child: const Align(
+                        child: Text(
+                          "4", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
                       ),
                     ),
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        color: Colors.brown,
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        color: Colors.yellow,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.purple,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                    
+                  )
+                ],
+              ),
             ),
-          ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Expanded(flex: 3,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 127, 194, 157),
+                      child: const Align(
+                        child: Text(
+                          "5", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
+                      ),
+                    ),
+                    
+                    ),
+                  Expanded(flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 85, 107, 175),
+                      child: const Align(
+                        child: Text(
+                          "6", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
+                      ),
+                    ),
+                    
+                  ),
+                  Expanded(flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 220, 118, 101),
+                      child: const Align(
+                        child: Text(
+                          "7", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
+                      ),
+                    ),
+                    
+                  ),
+                  Expanded(flex: 3,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: const Color.fromARGB(255, 54, 158, 127),
+                      child: const Align(
+                        child: Text(
+                          "8", 
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                          textAlign: TextAlign.center,
+                          ),
+                      ),
+                    ),
+                    
+                    ),
+                ]
+              ),
+            ),
+          ],
         ),
       ),
     );
